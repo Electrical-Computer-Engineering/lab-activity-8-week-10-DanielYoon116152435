bf46d6e42b10f402923510824fcbe2d257ef7b72
Daniel Yoon <eunsang.yoon@stonybrook.edu>
2024-04-02 17:16:36 -0400

    First Submission

--- part1_question1.c
+++ part1_question1.c
@@ -1,1 +1,161 @@
+// Dnaiel Yoon 116152435
 
+#include <stdio.h>
+#define MAX 32
+
+int my_isalpha(char ch)
+{
+    if (ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int my_isdigit(char ch)
+{
+    if (ch >= '0' && ch <= '9')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int my_is_upper(char ch)
+{
+    if (ch >= 'A' && ch <= 'Z')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+char my_to_upper(char ch)
+{
+    if (ch >= 'a' && ch <= 'z')
+    {
+        ch = ch - 'a' + 'A';
+        return ch;
+    }
+    else
+    {
+        return ch;
+    }
+}
+
+char my_to_lower(char ch)
+{
+    if (ch >= 'A' && ch <= 'Z')
+    {
+        ch = ch - 'A' + 'a';
+        return ch;
+    }
+    else
+    {
+        return ch;
+    }
+}
+
+int main()
+{
+    printf("Command\n1-check if the input is alphabet.\n2-check if the input is digit.\n3-Check if the input is upper case letter.\n4-Switch the input into upper case if it is lower case.\n5-Switch the input into lower case if it is upper case.\n6-Re-enter the input.\n7-To END THE PROGRAM.\n");
+    char array[MAX];
+    int command;
+    printf("Enter an input: ");
+    fgets(array, sizeof(array), stdin);
+
+    while (1)
+    {
+        printf("Enter the command number: ");
+        scanf("%d", &command);
+        getchar();
+
+        if (command == 1)
+        {
+            int i = 0;
+            for (i; array[i] != '\0'; i++)
+            {
+                if (my_isalpha(array[i]))
+                {
+                    printf("%c is a letter.\n", array[i]);
+                }
+                if (!my_isalpha(array[i]))
+                {
+                    printf("%c is not a letter.\n", array[i]);
+                }
+            }
+        }
+        else if (command == 2)
+        {
+            int i = 0;
+            for (i; array[i] != '\0'; i++)
+            {
+                if (my_isdigit(array[i]))
+                {
+                    printf("%c is a digit.\n", array[i]);
+                }
+                if (!my_isdigit(array[i]))
+                {
+                    printf("%c is not a digit.\n", array[i]);
+                }
+            }
+        }
+        else if (command == 3)
+        {
+            int i = 0;
+            for (i; array[i] != '\0'; i++)
+            {
+                if (my_is_upper(array[i]))
+                {
+                    printf("%c is an Upper case letter.\n", array[i]);
+                }
+                if (!my_is_upper(array[i]))
+                {
+                    printf("%c is not an Upper case letter.\n", array[i]);
+                }
+            }
+        }
+        else if (command == 4)
+        {
+            int i = 0;
+            for (i; array[i] != '\0'; i++)
+            {
+                printf("%c", my_to_upper(array[i]));
+            }
+        }
+        else if (command == 5)
+        {
+            int i = 0;
+            for (i; array[i] != '\0'; i++)
+            {
+                printf("%c", my_to_lower(array[i]));
+            }
+        }
+        else if (command == 6)
+        {
+            printf("Re-enter the input: ");
+            fgets(array, sizeof(array), stdin);
+        }
+        else if (command == 7)
+        {
+            printf("Program ENDS.\n");
+            break;
+        }
+        else
+        {
+            printf("wrong command. Try agian.\n");
+            printf("Enter the command number: ");
+            scanf("%d", &command);
+            getchar();
+        }
+    }
+}
--- part2_question1.c
+++ part2_question1.c
@@ -1,1 +1,172 @@
+// Dnaiel Yoon 116152435
 
+#include <stdio.h>
+#define MAX 32
+
+int my_strcmp(char str1[], char str2[])
+{
+    int i = 0;
+    while (str1[i] != '\0' && str2[i] != '\0')
+    {
+        if (str1[i] == str2[i])
+        {
+            i++;
+        }
+        else
+        {
+            i = 0;
+            break;
+        }
+    }
+    if (i != 0)
+        return 1;
+    if (i == 0)
+        return 0;
+}
+
+int my_strncmp(char str1[], char str2[], int n)
+{
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (str1[i] == str2[i])
+        {
+            count++;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    if (count == n)
+        return 0;
+}
+
+char *my_strcpy(char dest[], char src[])
+{
+    int i = 0;
+    while (src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0'; // null terminating
+    return dest;
+}
+
+char *my_strcat(char dest[], char src[])
+{
+    int i = 0;
+    while (dest[i] != '\0')
+    {
+        i++;
+    }
+    
+    int j;
+    for (j = 0; src[j] != '\0'; j++)
+    {
+        dest[i] = src[j];
+        i++;
+    }
+
+    dest[i] = '\0'; // null-terminating
+    printf("in concar %s", dest);
+    return dest;
+}
+
+char *my_reverse(char dest[], char src[])
+{
+    int i = 0;
+    while (src[i] != '\0')
+    {
+        i++;
+    }
+    int k = 0;
+    for (int j = i - 1; j >= 0; j--)
+    {
+        dest[k++] = src[j];
+    }
+    dest[k] = '\0';
+    return dest;
+}
+
+int main()
+{
+    printf("Command\n1-Check if both inputs are equal.\n2-Check if first n characters of both inputs are equal.\n3-Copies the second string into the first string.\n4-Append the second string at the end of the first string.\n5-Stores the reversed second string into the first string.\n6-To END THE PROGRAM.\n");
+    char str1[MAX];
+    char str2[MAX];
+    int command;
+    printf("Enter inputs.\n");
+    printf("The first string: ");
+    fgets(str1, sizeof(str1), stdin);
+
+    printf("The second string: ");
+    fgets(str2, sizeof(str2), stdin);
+
+    while (1)
+    {
+        printf("Enter the command number: ");
+        scanf("%d", &command);
+        getchar();
+        int n = 0;
+
+        if (command == 1)
+        {
+            if (!my_strcmp(str1, str2))
+            {
+                printf("Two strings are equal as "
+                       "%s"
+                       "\n",
+                       str1);
+            }
+            else if (my_strcmp(str1, str2))
+            {
+                printf("Two strings are different.\n");
+            }
+        }
+        else if (command == 2)
+        {
+            printf("Enter the first (n) characters to compare: ");
+            scanf("%d", &n);
+            getchar();
+
+            if (!my_strncmp(str1, str2, n))
+            {
+                printf("The first %d characters of both strings are the same.\n", n);
+            }
+            else if (my_strncmp(str1, str2, n))
+            {
+                printf("The first %d characters of both strings are not the same.\n", n);
+            }
+        }
+        else if (command == 3)
+        {
+            printf("Updated string1:\n");
+            printf("%s\n", my_strcpy(str1, str2));
+        }
+        else if (command == 4)
+        {
+            printf("Updated string1:\n");
+            my_strcat(str1, str2);
+            printf("%s", str1);
+        }
+        else if (command == 5)
+        {
+            printf("Updated string1:\n");
+            my_reverse(str1, str2);
+            printf("%s", str1);
+        }
+        else if (command == 6)
+        {
+            printf("Program ENDS.\n");
+            break;
+        }
+        else
+        {
+            printf("wrong command. Try agian.\n");
+            printf("Enter the command number: ");
+            scanf("%d", &command);
+            getchar();
+        }
+    }
+}
